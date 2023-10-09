2658885165c34eb4e6417bf0fb56037dc65e5291
MollaMohammadIbrahim <mmibrahim.cseru@gmail.com>
2023-10-10 04:23:08 +0600

    date validation in any where in the string

--- dateValidation.cpp
+++ dateValidation.cpp
@@ -13,23 +13,28 @@ int main() {
     int n;
     // mail.length is at most 100
  
-    std::regex pattern(R"(\b((0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[0-2])-([1-2][0-9]{3}))\b)");
-
-   
+    std::regex pattern(R"(((0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[0-2])-([1-2][0-9]{3})))");
     smatch match;
     cin>>n;
     set <string> emals;
     string s;
+    char a;
     while(n--){
+        
         getline(cin,s);
-        if(regex_match(s,match,pattern)){
-            emals.insert(match[0]);
-            cout<<"found"<<endl;
-            //cout<<match.position()<<endl;
+        auto start = sregex_iterator(s.begin(),s.end(),pattern);
+        auto end = sregex_iterator();
+        if(start != end)cout<<"found "<<endl;
+        for( auto k = start; k != end; ++k){
+            match = *k;
+            cout<<match.str()<<endl;
         }
-        else cout<<"notMatched" << endl;
+        // if(regex_match(s,match,pattern)){
+        //     cout<<"found"<<endl;
+        // }
+        // else cout<<"unmatched"<<endl;
     } 
-    
+
     cout<<endl;
     return 0;
 }
